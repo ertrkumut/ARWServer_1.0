6233f2849ecfed47252c2b5d3ffaf334caa779a7
Umutcan Erturk <powerLEd@Umutcans-MacBook-Pro.local>
2017-02-17 03:48:14 +0200

    Simple Connection

---
+++ ARVServer.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"os"
+)
+
+const (
+	SUCCESS = "Success"
+)
+
+func main() {
+
+	fmt.Println("Launching server... ")
+
+	ln, err := net.Listen("tcp", ":8081")
+
+	if err != nil {
+		fmt.Println("Error listening...")
+		os.Exit(1)
+	}
+	defer ln.Close()
+
+	for {
+
+		conn, acceptErr := ln.Accept()
+
+		if acceptErr != nil {
+			fmt.Println("Error Accepting...")
+			os.Exit(1)
+		}
+
+		requestBytes := make([]byte, 1024)
+
+		_, err := conn.Read(requestBytes)
+		if err != nil {
+			println("Write to server failed:", err.Error())
+			os.Exit(1)
+		}
+
+		HandleRequests(requestBytes, &conn)
+	}
+}
+
+func ConnectionEvent(conn *net.Conn) {
+	clientConn := *conn
+	clientConn.Write([]byte("ConnectionSuccess"))
+}
+
+func HandleRequests(reqBytes []byte, conn *net.Conn) {
+	reqBytes = bytes.Trim(reqBytes, "\x00")
+	reqData := string(reqBytes)
+
+	if reqData != "" {
+		if reqData == SUCCESS {
+			ConnectionEvent(conn)
+		}
+	}
+}
