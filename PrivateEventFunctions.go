9dba3296bd4e4e9cef11e85a149f4d89198b36c3
Umutcan Erturk <powerLEd@Umutcans-MacBook-Pro.local>
2017-03-01 19:02:06 +0200

    Private Event Handlers

---
+++ PrivateEventFunctions.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func P_ConnectionSuccess(arwServer *ARWServer, conn net.Conn, arwObj ARWObject) {
+	arwServer.sessions.StartSession(&conn)
+
+	conn.Write(arwObj.Compress())
+}
+
+func P_LoginEvent(arwServer *ARWServer, conn net.Conn, arwObj ARWObject) {
+	userName := arwObj.evntParams.GetString("userName")
+
+	newUser := arwServer.userManager.CreateUser(userName, conn, arwServer)
+	fmt.Printf("User Login Server : %s id : %d Session : %s \n", newUser.name, newUser.id, newUser.session.GetConnString())
+}
