954bcf29b4a6defd1997efbd25f848669f6fa7c0
Umutcan Erturk <powerLEd@Umutcans-MacBook-Pro.local>
2017-03-02 16:17:05 +0200

    Server Backend Handlers

--- PrivateEventFunctions.go
+++ PrivateEventFunctions.go
@@ -8,16 +8,23 @@ import (
 func P_ConnectionSuccess(arwServer *ARWServer, conn net.Conn, arwObj ARWObject) {
 	arwServer.sessions.StartSession(&conn)
 
-	conn.Write(arwObj.Compress())
+	arwServer.SendRequestWithConn(conn, arwObj)
 }
 
 func P_LoginEvent(arwServer *ARWServer, conn net.Conn, arwObj ARWObject) {
-	userName := arwObj.evntParams.GetString("userName")
+	userName := arwObj.eventParams.GetString("userName")
 
 	newUser := arwServer.userManager.CreateUser(userName, conn, arwServer)
 	fmt.Printf("User Login Server : %s id : %d Session : %s \n", newUser.name, newUser.id, newUser.session.GetConnString())
 
-	if arwServer.events.User_Login_Event.Handler != nil {
-		arwServer.events.User_Login_Event.Handler(arwObj)
+	var newArwObj ARWObject
+	newArwObj.requestName = Login
+	newArwObj.eventParams.PutString("userName", newUser.name)
+	newArwObj.eventParams.PutInt("userId", newUser.id)
+
+	if arwServer.events.Login.Handler != nil {
+		arwServer.events.Login.Handler(newArwObj)
 	}
+
+	arwServer.SendRequestWithConn(conn, newArwObj)
 }
