954ef364b706a1862a92f91bc9916010770bb053
Umutcan Erturk <powerLEd@Umutcans-MacBook-Pro.local>
2017-04-06 18:56:09 +0300

    Add Extension Handler

--- ExampleServer.go
+++ ExampleServer.go
@@ -1,9 +1,40 @@
 package main
 
+import "fmt"
+
+var arwServer ARWServer
+
 func main() {
-	var arwServer ARWServer
+
 	arwServer.Initialize()
 
 	// arwServer.AddEventHandler(&arwServer.events.Login, Loginhandler)
+
+	arwServer.AddExtensionHandler("Hello", HelloHandler)
+	arwServer.AddEventHandler(&arwServer.events.Room_Create, RoomCreatehandler)
 	arwServer.ProcessEvents()
+}
+
+func HelloHandler(arwServer *ARWServer, user *User, obj ARWObject) {
+	fmt.Println(user.name, obj.GetString("Deneme"))
+}
+
+func RoomCreatehandler(obj ARWObject) {
+	roomId := obj.eventParams.GetInt("RoomId")
+
+	room, err := arwServer.roomManager.FindRoomWithID(roomId)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Room Create : ", roomId)
+
+	room.InitializeMethod = RoomInitialize
+}
+
+func RoomInitialize(server *ARWServer, room *Room) {
+	room.AddExtensionHandler("Hello", HelloHandler)
+	fmt.Println(room.name, "intialize")
 }
