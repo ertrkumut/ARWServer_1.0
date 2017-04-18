da25a75862aa6f2645b6042c58a79945c91a4a55
Umutcan Erturk <powerLEd@Umutcans-MacBook-Pro.local>
2017-04-18 17:22:11 +0300

    Delete user after disconnection

--- Sessions.go
+++ Sessions.go
@@ -19,14 +19,23 @@ func (s *SessionManager) SendRequestToAllSessions(arwServer *ARWServer, obj ARWO
 	}
 }
 
-func (s *SessionManager) CloseSession(conn net.Conn) {
-	newArray := make([]Session, 0, len(s.allSessions)-1)
+func (s *SessionManager) CloseSession(arw *ARWServer, conn net.Conn) {
+	newSessionsArray := make([]Session, 0, len(s.allSessions)-1)
+	newUsersArray := make([]User, 0, len(arw.userManager.allUsers)-1)
 
 	for ii := 0; ii < len(s.allSessions); ii++ {
 		if s.allSessions[ii].GetConn() != conn {
-			newArray = append(newArray, s.allSessions[ii])
+			newSessionsArray = append(newSessionsArray, s.allSessions[ii])
 		}
 	}
-	s.allSessions = newArray
+	s.allSessions = newSessionsArray
+
+	for ii := 0; ii < len(arw.userManager.allUsers); ii++ {
+		if arw.userManager.allUsers[ii].session.GetConn() != conn {
+			newUsersArray = append(newUsersArray, arw.userManager.allUsers[ii])
+		}
+	}
+	arw.userManager.allUsers = newUsersArray
+
 	conn.Close()
 }
