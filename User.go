dd3819c7abb1a28ddc915654dbdb6b2e15d076d4
Umutcan Erturk <powerLEd@Umutcans-MacBook-Pro.local>
2017-06-23 16:52:09 +0300

    Add User Variable

--- User.go
+++ User.go
@@ -3,19 +3,38 @@ package main
 import "strconv"
 
 type User struct {
-	name     string
-	id       int
-	lastRoom Room
-	session  Session
+	name         string
+	id           int
+	lastRoom     *Room
+	userVariable []*UserVariable
+	session      Session
 }
 
-func (u *User) GetDataForOtherUser(user *User) string {
-	userData := u.name + "^^" + strconv.Itoa(u.id) + "^^"
-	if u != user {
+func (user *User) GetDataForOtherUser(otherUser *User) string {
+	userData := user.name + "^^" + strconv.Itoa(user.id) + "^^"
+	if user != otherUser {
 		userData += "false" // IsMe true ^^ false
 	} else {
 		userData += "true"
 	}
 
 	return userData
+}
+
+func (user *User) AddUserVariable(variables []*UserVariable) {
+	varIsExist := false
+
+	for _, newVariable := range variables {
+		for _, userVar := range user.userVariable {
+			if newVariable.key == userVar.key {
+				varIsExist = true
+				userVar.value = newVariable.value
+				break
+			}
+		}
+
+		if !varIsExist {
+			user.userVariable = append(user.userVariable, newVariable)
+		}
+	}
 }
