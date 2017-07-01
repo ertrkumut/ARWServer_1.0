760cbef72a3f083890cbdf1be6476d285cb087ef
ertrkumut <u.c.erturk@gmail.com>
2017-07-01 20:15:47 +0300

    Compress User Variables to string list

--- User.go
+++ User.go
@@ -16,23 +16,31 @@ type User struct {
 func (user *User) GetUserDataToString() string {
 	userData := user.name + "^^" + strconv.Itoa(user.id) + "^^"
 
+	userData += user.CompressUserVariables()
+
+	return userData
+}
+
+func (user *User) CompressUserVariables() string {
+	userData := ""
 	for _, userVar := range user.userVariables {
 		userData += userVar.key + "½" + userVar.value + "§"
 	}
 	userData = strings.TrimRight(userData, "#")
+
 	return userData
 }
 
-func (user *User) VariableIsExist(key string) bool {
+func (user *User) VariableIsExist(key string) (bool, *UserVariable) {
 	variableIsExist := false
 
 	for _, userVar := range user.userVariables {
 		if userVar.key == key {
 			variableIsExist = true
-			return variableIsExist
+			return variableIsExist, userVar
 		}
 	}
-	return variableIsExist
+	return variableIsExist, nil
 }
 
 func (user *User) AddUserVariables(variables []*UserVariable) {
@@ -42,13 +50,31 @@ func (user *User) AddUserVariables(variables []*UserVariable) {
 		for _, userVar := range user.userVariables {
 			if newVariable.key == userVar.key {
 				varIsExist = true
-				userVar.value = newVariable.value
+				userVar.value = newVariable.value // Send User Variable Update func.
 				break
 			}
 		}
 
 		if !varIsExist {
 			user.userVariables = append(user.userVariables, newVariable)
+		}
+	}
+}
+
+func (user *User) AddStringUserVariable(key string, value string) {
+	varIsExist, existVar := user.VariableIsExist(key)
+
+	if !varIsExist {
+		var newVariable *UserVariable
+		newVariable = new(UserVariable)
+
+		newVariable.key = key
+		newVariable.value = value
+
+		user.userVariables = append(user.userVariables, newVariable)
+	} else {
+		if existVar.GetValueToString() != value {
+			existVar.value = value
 		}
 	}
 }
