36c72f88e96b05f36b6a860ea681899101e26f78
ertrkumut <u.c.erturk@gmail.com>
2017-07-04 14:17:51 +0300

    User Variable Update

--- ARWEvents.go
+++ ARWEvents.go
@@ -13,16 +13,17 @@ type ARWEvents struct {
 }
 
 const (
-	Exception_Error    = "EXCEPTION_ERROR"
-	Connection_Success = "CONNECTION_SUCCESS"
-	Login              = "LOGIN"
-	Login_Error        = "LOGIN_ERROR"
-	Room_Create        = "ROOM_CREATE"
-	Join_Room          = "ROOM_JOIN"
-	User_Enter_Room    = "USER_ENTER_ROOM"
-	Disconnection      = "DISCONNECTION"
-	User_Exit_Room     = "USER_EXIT_ROOM"
-	Extension_Response = "EXTENTION_REQUEST"
+	Exception_Error      = "EXCEPTION_ERROR"
+	Connection_Success   = "CONNECTION_SUCCESS"
+	Login                = "LOGIN"
+	Login_Error          = "LOGIN_ERROR"
+	Room_Create          = "ROOM_CREATE"
+	Join_Room            = "ROOM_JOIN"
+	User_Enter_Room      = "USER_ENTER_ROOM"
+	User_Variable_Update = "USER_VARIABLE_UPDATE"
+	Disconnection        = "DISCONNECTION"
+	User_Exit_Room       = "USER_EXIT_ROOM"
+	Extension_Response   = "EXTENTION_REQUEST"
 )
 
 func (events *ARWEvents) Initialize() {
